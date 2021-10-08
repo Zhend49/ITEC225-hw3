ce551fea9adccf0ca3ca1ad04f8e6968cf4eccdf
Zhend49 <90920001+Zhend49@users.noreply.github.com>
2021-10-08 13:33:53 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+
+// part 1
+function computeForce() {
+    // code
+
+}
+
+
+// part 2
+function computeAvgVelocity() {
+    // code
+
+}
+
+
+// part 3
+function computePrincipal() {
+    // code
+
+}
+
+// part 4
+function computePresentValue() {
+    // code
+
+}
